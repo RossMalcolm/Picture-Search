7778041f6bbe9fcb06814f3f84cceefc1c81aa81
Ross Malcolm <rossmalc75@gmail.com>
2019-10-15 20:07:41 -0400

    Load Image List

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL: 'https://api.unsplash.com/',
-
     headers:{
         Authorization: 
             'Client-ID 66c633ba130eed93696be0e197afc8fe1fac5ac2dc039aa37eb8bc9ec4f0aab4'
